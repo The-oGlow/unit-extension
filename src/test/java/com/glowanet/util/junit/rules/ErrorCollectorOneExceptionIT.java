26c540b83cdd63a268c217fc1da6ab124a41e0fc
ollily <coding@glowa-net.com>
2022-05-27 13:12:26 +0200

    #666 UNIT: IT test for ErrorCollector
    Signed-off-by: ollily <coding@glowa-net.com>

---
+++ src/test/java/com/glowanet/util/junit/rules/ErrorCollectorOneExceptionIT.java
@@ -0,0 +1,31 @@
+package com.glowanet.util.junit.rules;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ErrorCollectorOneExceptionIT {
+
+    @Rule
+    public ErrorCollectorExt collector = new ErrorCollectorExt();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testWithException() {
+        collector.checkThat(20, equalTo(1));
+
+        expectedException.expect(AssertionError.class);
+    }
+
+    @After
+    public void tearDown() {
+        assertThat(collector.getErrorSize(), equalTo(1));
+    }
+
+}
